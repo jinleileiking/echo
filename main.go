d73467c4509b75c999cfd43dc3ac00e0531b13a2
jinleileiking <jinleileiking@gmail.com>
2021-02-04 14:57:30 +0800

    add long delay

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func echo(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Printf("incoming URL: %#v\n ", req.URL)
+	time.Sleep(time.Duration(60) * time.Second)
 	k, _ := req.URL.Query()["k"]
 	fmt.Fprintf(w, fmt.Sprintf("%s\n", k[0]))
 }
